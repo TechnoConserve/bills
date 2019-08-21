2db1403e755b01b80cddfb1f5d2219786b1e321c
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-08-21 17:33:34 -0600

    Display Total Owed And Individual Bill Totals
    Index view is now passed two dictionaries using Housemates as keys.
    
    total_owed is a dictionary with values representing the total owed per
    for that particular housemate.
    
    individual_bills is a dictionary containing a list of all unpaid bills
    so that a categorical breakdown of the owed total can be shown per
    housemate.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
-from db import db, get_housemates, get_gas_total, get_internet_total, get_power_total, get_water_total
+from db import db, get_housemates, get_gas_total, get_internet_total, get_power_total, get_water_total, \
+    get_total_owed, get_individual_unpaid_bills
 
 app = Flask(__name__)
 
@@ -8,13 +9,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     housemates = get_housemates()
+    total_owed = {}
+    individual_bills = {}
+    for housemate in housemates:
+        owed = get_total_owed(housemate)
+        if owed is not None:
+            total_owed[housemate] = owed / 100
+        else:
+            total_owed[housemate] = 0
+
+        individual_bills[housemate] = get_individual_unpaid_bills(housemate)
+
     gas_total = get_gas_total() / 100
     internet_total = get_internet_total() / 100
     power_total = get_power_total() / 100
     water_total = get_water_total() / 100
 
     return render_template("index.html", gas_total=gas_total, internet_total=internet_total, power_total=power_total,
-                           water_total=water_total, housemates=housemates)
+                           water_total=water_total, housemates=housemates, total_owed=total_owed,
+                           individual_bills=individual_bills)
 
 
 @app.before_request
