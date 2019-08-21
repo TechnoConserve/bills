f8c8419390ed5ebba52040c4a9f384e4fd140ddc
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-08-21 14:07:48 -0600

    Web View Formatting Update
    Bill totals are now divided by 100 before being sent to the HTML
    template so they can be displayed in dollars and cents.
    
    Added formatting to the index HMTL template so totals are displayed with
    two decimal places.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- web.py
+++ web.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     housemates = get_housemates()
-    gas_total = get_gas_total()
-    internet_total = get_internet_total()
-    power_total = get_power_total()
-    water_total = get_water_total()
+    gas_total = get_gas_total() / 100
+    internet_total = get_internet_total() / 100
+    power_total = get_power_total() / 100
+    water_total = get_water_total() / 100
 
     return render_template("index.html", gas_total=gas_total, internet_total=internet_total, power_total=power_total,
                            water_total=water_total, housemates=housemates)
