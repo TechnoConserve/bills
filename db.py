030663f557482a8652cbc6882a4031af66c3fc96
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-08-20 23:28:04 -0600

    Updated Models
    Renamed HouseMate to Housemate.
    
    Defined __repr__ and __str__ methods so make displaying displaying their
    info in jinja templates more meaningful.
    
    Removed unique constraint from the category property of IndividualBill.
    
    Added functions to help create tables, get housemates, and get the sum
    of individual gas bills.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- db.py
+++ db.py
@@ -10,24 +10,44 @@ class BaseModel(Model):
         database = db
 
 
-class HouseMate(BaseModel):
+class Housemate(BaseModel):
     name = CharField(unique=True)
     email = CharField(unique=True)
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
 
     def amount_owed(self):
         pass
 
 
 class IndividualBill(BaseModel):
-    debtor = ForeignKeyField(HouseMate, backref="bills")
+    debtor = ForeignKeyField(Housemate, backref="bills")
 
     # Service Period
     service_start = DateField(null=False)
     service_end = DateField(null=False)
 
-    category = CharField(choices=categories, null=False, unique=True)
+    category = CharField(choices=categories, null=False)
 
     # Total cost per individual using fixed-point representation
     total = IntegerField(null=False)
 
     paid = BooleanField(default=False)
+
+
+def create_tables():
+    with db:
+        db.create_tables([Housemate, IndividualBill])
+
+
+def get_housemates():
+    return Housemate.select()
+
+
+def get_gas_total():
+    return IndividualBill.select(fn.SUM(IndividualBill.total))\
+        .where(IndividualBill.category == categories[0][0]).scalar()
