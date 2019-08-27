f59dfb248b783987efdef2f8c97e89001b01b940
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-08-27 15:14:16 -0600

    Bill Cards With Dates
    Passed index examples of individual bills so service dates could be
    displayed for each category of unpaid bill.
    
    Bill categories are now stylized as cards.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-from db import db, get_housemates, get_gas_total, get_internet_total, get_power_total, get_water_total, \
+from db import categories, db, get_housemates, get_gas_total, get_internet_total, get_power_total, get_water_total, \
     get_total_owed, get_individual_unpaid_bills
 
 app = Flask(__name__)
@@ -20,6 +20,26 @@ def index():
 
         individual_bills[housemate] = get_individual_unpaid_bills(housemate)
 
+    # Get an individual bill of each category
+    gas_bill = None
+    internet_bill = None
+    power_bill = None
+    water_bill = None
+    for housemate, ind_bill_lst in individual_bills.items():
+        for bill in ind_bill_lst:
+            if bill.category == categories[0][0]:
+                gas_bill = bill
+            elif bill.category == categories[1][0]:
+                internet_bill = bill
+            elif bill.category == categories[2][0]:
+                power_bill = bill
+            elif bill.category == categories[3][0]:
+                water_bill = bill
+
+        # Break the loop if we have an example of all categories
+        if gas_bill is not None and internet_bill is not None and power_bill is not None and water_bill is not None:
+            break
+
     gas_total = get_gas_total() / 100
     internet_total = get_internet_total() / 100
     power_total = get_power_total() / 100
@@ -27,7 +47,8 @@ def index():
 
     return render_template("index.html", gas_total=gas_total, internet_total=internet_total, power_total=power_total,
                            water_total=water_total, housemates=housemates, total_owed=total_owed,
-                           individual_bills=individual_bills)
+                           individual_bills=individual_bills, gas_bill=gas_bill, internet_bill=internet_bill,
+                           power_bill=power_bill, water_bill=water_bill)
 
 
 @app.before_request
